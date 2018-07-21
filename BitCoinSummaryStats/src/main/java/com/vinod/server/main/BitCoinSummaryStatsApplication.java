d938136d62ed0d070629b158a75ce7899ec3620b
kvkumar2112 <vinodkumar@10.245.8.221>
2018-07-21 14:13:51 +0530

    Initial Commit of Bit Coin Rest API

---
+++ BitCoinSummaryStats/src/main/java/com/vinod/server/main/BitCoinSummaryStatsApplication.java
@@ -0,0 +1,12 @@
+package com.vinod.server.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitCoinSummaryStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitCoinSummaryStatsApplication.class, args);
+	}
+}
